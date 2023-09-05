d872b3f6f544ff7a8af93081ce1036f3863ba5c0
ilhammfauzii <144036781+ilhammfauzii@users.noreply.github.com>
2023-09-05 17:58:34 +0700

    Update main.yml
    Menambahkan script

---
+++ Aplikasi-Login-01-starter-study-case/assets/scripts/login-script.js
@@ -0,0 +1,33 @@
+/*
+ * Catatan:
+ * Gunakan fungsi goToHome() untuk menampilkan halaman home
+ * Gunakan fungsi goToLogin() untuk menampilkan halaman login
+ * Gunakan fungsi showPopUp() untuk menampilkan pop up error
+ */
+
+const loginFormElement = document.querySelector('#loginForm');
+const inputEmailElement = document.querySelector('#inputEmail');
+const inputPasswordElement = document.querySelector('#inputPassword');
+
+const expectedEmail = 'admin@dicoding.com';
+const expectedPassword = 'superpassword';
+
+/* Menambahkan aksi klik pada button */
+loginFormElement.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+/* Mendapatkan nilai dari masing-masing input (email dan password) ketika tombol ditekan. */
+    const email = inputEmailElement.value;
+    const password = inputPasswordElement.value;
+
+/* Memastikan bahwa nilai email dan password sesuai dengan nilai yang tersimpan*/
+    if (email == expectedEmail && password == expectedPassword) {
+
+/* Jika sesuai maka program akan berpindah ke halaman home. */
+      goToHome();
+    } else {
+
+/* Namun jika tidak sesuai maka akan menampilkan informasi bahwa input salah. */
+      showPopUp();
+    }
+});
